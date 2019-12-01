8169f578b7317b9eed1079a92c7548dfbe699b53
polferrando98 <pol.ferrando.98@gmail.com>
2019-12-01 23:01:18 +0100

    Replication wip

--- Multiplayer Game/ReplicationCommand.h
+++ Multiplayer Game/ReplicationCommand.h
@@ -1,2 +1,16 @@
 #pragma once
 
+enum class ReplicationAction {
+	None,
+	Create,
+	CreateClients,
+	Update,
+	Destroy
+};
+
+struct ReplicationCommand
+{
+	ReplicationAction action;
+	uint32 networkId;
+};
+
